37547d4ac9657784300a684b58898c8b042594c6
dgamliel <dgamliel7@gmail.com>
2020-12-27 13:31:58 -0600

    changed card from bootstrap to basic css

--- client/src/components/results.js
+++ client/src/components/results.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState} from "react";
 import { Card } from "react-bootstrap";
 import './results.css';
 
@@ -29,27 +29,66 @@ const capitalizeFirstLetter = (inStr) => {
     return inStr.charAt(0).toUpperCase() + inStr.slice(1);
 }
 
+const classFromClickedState = (defaultState, clickedState, clicked) => {
+    return clicked === true ? clickedState : defaultState;
+}
+
+const bgFromSeverity = (severity) => {
+    return severity === undefined || severity === null ? '' : severity;
+}
+
 const Result = (props) => {
+
+    const [clicked, setClicked] = useState(false);
+    const handleClick = (e) => {
+        setClicked(!clicked);
+    }
+
+
+
     console.log(`Props are ${JSON.stringify(props)}`);
 
     const {file,index, severity, issues} = props.info;
 
     console.log(`Info passed in ${JSON.stringify(props.info, null, 2)}`);
     return (
-        <div style={center}>
-            <Card border={getBorder(severity)} style={cardWidth}>
-                <Card.Header>{file}</Card.Header>
-                <Card.Body >
-                    <Card.Title>Severity: {capitalizeFirstLetter(severity)}</Card.Title>
-                    <Card.Text>Byte Offset: <code>{index}</code></Card.Text>
+        <>
+            {/* <div style={center}>
+                <Card border={getBorder(severity)} style={cardWidth}>
+                    <Card.Header>{file}</Card.Header>
+                    <Card.Body >
+                        <Card.Title>Severity: {capitalizeFirstLetter(severity)}</Card.Title>
+                        <Card.Text>Byte Offset: <code>{index}</code></Card.Text>
 
-                    <Card.Link href={issues}>
-                    {file} security issues
-                    </Card.Link>
-                </Card.Body>
+                        <Card.Link href={issues}>
+                        {file} security issues
+                        </Card.Link>
+                    </Card.Body>
 
-            </Card>
-        </div>
+                </Card>
+            </div> */}
+
+            <div className={classFromClickedState("custom-box", "custom-box expanded", clicked)} 
+             onClick={handleClick}
+            >
+                <h3 className="box-header">{file}</h3>
+                <div className={classFromClickedState("box-body", "box-body expanded", clicked)}>
+                    <div style={{textAlign: 'center'}}>
+                        Severity &mdash; 
+                        <span className={bgFromSeverity(severity)}>{capitalizeFirstLetter(severity)}</span>
+                    </div>
+                    <ul style={{marginTop: '5px'}}>
+                        <li>
+                            Byte Offset &mdash; <code>{index}</code>
+                        </li>
+                        <li>
+                            <a href={issues}>{file}</a> Security Issues
+                        </li>
+                    </ul>
+                        
+                </div>
+            </div>
+        </>
     );
 }
 
